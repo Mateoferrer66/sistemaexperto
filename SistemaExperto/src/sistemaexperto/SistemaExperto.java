a499c3a359f8c6a5f6c14065b7b56d743f3e7e53
Mateoferrer66 <mateoferrer66@gmail.com>
2023-05-24 12:26:18 -0500

    Initial commit

---
+++ SistemaExperto/src/sistemaexperto/SistemaExperto.java
@@ -0,0 +1,16 @@
+package sistemaexperto;
+
+/**
+ *
+ * @author Mateo Ferrer
+ */
+public class SistemaExperto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new formularioSE().setVisible(true);
+    }
+    
+}
